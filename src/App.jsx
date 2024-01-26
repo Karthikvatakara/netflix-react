875feb9c606c744c1fea15ab063780de95626cf6
karthik <krishnankarthik07@gmail.com>
2024-01-26 09:04:52 +0530

    netflix final commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Home from "./Pages/Home"
+import { Route,Routes } from "react-router-dom"
+import { AuthContextProvider } from "./context/AuthContext"
+import Login from "./Pages/Login"
+import Signup from "./Pages/Signup"
+import Account from "./Pages/Account"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function App() {
+  
+  return (
+    <>
+    <AuthContextProvider>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element = {<Home/>} />
+      <Route path="/login" element = {<Login/>} />
+      <Route path="/signup" element = {<Signup/>} />
+      <Route path="/account" element={
+        <ProtectedRoute>
+          <Account />
+        </ProtectedRoute>
+      }  />
+    </Routes>
+    </AuthContextProvider>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Row.jsx
@@ -0,0 +1,42 @@
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import Movie from "./Movie";
+import { MdChevronLeft,MdChevronRight } from "react-icons/md";
+
+const Row = (({title,fetchUrl,rowId}) =>{
+    const [movies,setMovies] = useState([])
+
+    
+    useEffect(() =>{
+        axios.get(fetchUrl).then((response) =>{
+            setMovies(response.data.results)
+        })
+    },[fetchUrl])
+
+    const sliderLeft = () =>{
+        var slider= document.getElementById('slider'+rowId)
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+
+    const sliderRight = () =>{
+        var slider = document.getElementById('slider'+rowId)
+        slider.scrollLeft = slider.scrollLeft + 500 ;  
+    }
+
+return(
+    <>
+    <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+    <div className="relative flex items-center group">
+        <MdChevronLeft onClick={sliderLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40}/>
+        <div id={'slider'+rowId} className="w-full h-full overflow-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+        {movies.map((item,id) =>(
+            <Movie key={id} item={item}/>
+        ))}
+        </div>
+        <MdChevronRight onClick={sliderRight} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40}/>
+    </div>
+    </>
+)
+})
+
+export default Row
